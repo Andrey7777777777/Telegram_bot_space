c189df686c72adb690335acee6b0dd1fd512d038
Andrey7777777777 <117460052+Andrey7777777777@users.noreply.github.com>
2023-01-27 11:26:41 +0200

    убрал зависимости внешнего окружения
    убрал зависимости внешнего окружения

--- image_downloading.py
+++ image_downloading.py
@@ -1,17 +1,11 @@
-from datetime import time
 from os import path
 from pathlib import Path
 from urllib.parse import unquote, urlsplit
 import requests
-from environs import Env
 import telegram
 
 
-def publish_image_to_channel(filepath):
-    env = Env()
-    env.read_env()
-    tg_bot_token = env.str('TG_BOT_TOKEN')
-    tg_channel_id = env.str('TG_CHANNEL_ID')
+def publish_image_to_channel(filepath, tg_bot_token, tg_channel_id):
     bot = telegram.Bot(token=tg_bot_token)
     with open(filepath, 'rb') as photo:
         bot.send_photo(
@@ -20,14 +14,14 @@ def publish_image_to_channel(filepath):
             timeout=20.)
 
 
-
 def get_extension(url):
     file_path = unquote(urlsplit(url).path)
     _, extension = path.splitext(file_path)
     return extension
 
 
-def upload_image(image_url, image_name, params=None):
+def upload_image(image_url, image_name, image_folder, params=None):
+    Path(image_folder).mkdir(parents=True, exist_ok=True)
     response = requests.get(image_url, params=params)
     response.raise_for_status()
     with open(image_name, 'wb') as file:
